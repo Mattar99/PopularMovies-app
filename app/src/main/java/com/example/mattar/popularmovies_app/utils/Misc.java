50accdb5984b8903b833c7aa54e13f7b45739520
Ahmed Mattar <AceMatar@gmail.com>
2018-05-10 01:14:07 +0200

    implemented Utils Misc

--- app/src/main/java/com/example/mattar/popularmovies_app/utils/Misc.java
+++ app/src/main/java/com/example/mattar/popularmovies_app/utils/Misc.java
@@ -1,8 +1,23 @@
 package com.example.mattar.popularmovies_app.utils;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+
 /**
  * Created by Mattar on 5/10/2018.
  */
 
 public class Misc {
+
+    public static boolean isNetworkAvailable(Context context){
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        boolean state = false ;
+        if(connectivityManager!=null){
+            state = connectivityManager.getActiveNetworkInfo().isConnected();
+        }
+
+        return  state;
+
+    }
 }
