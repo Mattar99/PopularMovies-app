42bb6ecd9a3b6aa12f7c38c53a72a99cf724bd29
Ahmed Mattar <AceMatar@gmail.com>
2018-05-15 03:47:03 +0200

    implemented db contract and helper

---
+++ app/src/main/java/com/example/mattar/popularmovies_app/data/MoviesProvider.java
@@ -0,0 +1,66 @@
+package com.example.mattar.popularmovies_app.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Mattar on 5/15/2018.
+ */
+
+public class MoviesProvider extends ContentProvider {
+
+    private MoviesDbHelper mOpenHelper;
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    public static final int MOVIES = 100;
+    public static final int MOVIE_WITH_ID = 101;
+
+
+
+    public static UriMatcher buildUriMatcher() {
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(MoviesContract.CONTENT_AUTHORITY, MoviesContract.PATH_MOVIES, MOVIES);
+        uriMatcher.addURI(MoviesContract.CONTENT_AUTHORITY, MoviesContract.PATH_MOVIES + "/#", MOVIE_WITH_ID);
+
+        return uriMatcher;
+    }
+
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
